f758ebdce6eb55da03276be71c07995d5536060e
dileep-reddy-git <dileepramaws@gmail.com>
2025-02-24 15:43:13 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_FQPYXqKuy', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7uuhatcg12mm0ur3f417t5l90', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_GMj1vz6Rq', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5a0tf7kr2t8kqiivr8b40j7nh1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
